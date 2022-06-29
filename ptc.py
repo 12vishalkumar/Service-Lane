9feebf0b91fa341aac061f2a87c55f21dd73f5be
12vishalkumar <90981381+12vishalkumar@users.noreply.github.com>
2022-06-29 11:41:00 +0530

    Add files via upload

---
+++ ptc.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+# Complete the 'serviceLane' function below.
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY cases
+def serviceLane(n, cases, width):
+    # Write your code here
+    arr = []
+    for i in cases:
+        A = []
+        for j in range(i[0], i[1]+1):
+            A.append(width[j])
+        arr.append(min(A))
+    return arr
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    t = int(first_multiple_input[1])
+    width = list(map(int, input().rstrip().split()))
+    cases = []
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+    result = serviceLane(n, cases, width)
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
